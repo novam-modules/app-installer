c4e277b6ea073710cf22b150e24646c3ffb6888b
Hilkiah Makemo <HILMAK01@GMAIL.COM>
2018-10-31 01:11:48 -0600

    Updated sidenav

--- resources/views/_partials/main/sidenav.blade.php
+++ resources/views/_partials/main/sidenav.blade.php
@@ -92,7 +92,7 @@
         </li>
         <li>
             <a href="#operations" aria-expanded="false" data-toggle="collapse">
-                <i class="fa fa-list fa-fw"></i> Operationsgit <i class="fa fa-sort-amount-asc" aria-hidden="true"></i>
+                 <i class="fa fa-sort-amount-asc" aria-hidden="true"></i> Operation
 
             </a>
             <ul id="operations" class="collapse list-unstyled ">
