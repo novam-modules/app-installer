4fc8af6e145acd8915325b524e935554a4fbb47e
Hilkiah Makemo <HILMAK01@GMAIL.COM>
2018-10-31 08:21:29 -0600

    Added new database tables for Security

---
+++ database/migrations/2018_10_31_131835_create_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('account_id')->default(-1);
+            $table->integer('group_id')->default(-1);
+            $table->string('name');
+            $table->text('info')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
---
+++ database/migrations/2018_10_31_132221_create_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('admin_id')->unsigned()->nullable();
+            $table->string('name');
+            $table->string('email');
+            $table->string('fein')->nullable();
+            $table->boolean('agree')->default(false);
+            $table->text('db')->nullable();
+            $table->text('mail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
