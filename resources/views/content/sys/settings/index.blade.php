7bfa5d16042fc9ac93d7a23c880e40e6d07d5d3f
Hilkiah Makemo <hilmak01@gmail.com>
2018-10-31 18:29:53 -0600

    Updates settings

--- resources/views/content/sys/settings/index.blade.php
+++ resources/views/content/sys/settings/index.blade.php
@@ -7,6 +7,25 @@
 @php
     dump($Configs);
 @endphp
+<div class="container">
+    <form>
+        <div class="form-group row">
+            <label for="inputName" class="col-sm-12 col-form-label"></label>
+            <div class="col-sm-12">
+                <input type="text" class="form-control" name="key" id="set-key" placeholder="">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputName" class="col-sm-12 col-form-label"></label>
+            <div class="col-sm-12">
+                <textarea class="form-control" name="key" id="set-key" placeholder="">
+            </div>
+            <div class="offset-sm-2 col-sm-10">
+                <button type="submit" class="btn btn-primary">Action</button>
+            </div>
+        </div>
+    </form>
+</div>
 <button>SUBMIT</button>
 </form>
 
